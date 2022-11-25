20ab93d96300106200c28b18f85315d3e41c741d
mario <viskol.mario@gmail.com>
2022-11-25 20:27:13 +0100

    aa

---
+++ vezba_20.11.py
@@ -0,0 +1,204 @@
+# print("Hello world!") # 1.1
+
+# a= 2
+# b= 4
+
+# print (a + b)
+# print (a - b)
+# print (a * b)   # 1.2
+
+# a= int(input("Unesite prvi broj: "))
+# b= int(input("Unesite drugi broj: "))
+
+# kolicnik = a / b
+# cel = a // b
+# step = a ** b
+
+# print("Kolicnik: ",kolicnik )
+# print("Celobrojno:",cel)
+# print("Stepenovanje: ",step) # 1.3
+
+# a = float(input("Unesite prvi broj: "))
+# b = float(input("Unesite drugi broj: "))
+
+
+# print("prvi broj",'%.2f' % a, "drugi broj: ",'%.3f' %b) 1.4
+
+# a = str(input("Unesite tekst1: "))
+
+# print("'{}'".format(a))   #1.4.2
+
+# a= input("unesi prvi: ")
+# b= input("unesi drugi: ")
+# print(a,',',b)               # 1.5
+
+# a=int(input("Unesite prvi ceo broj: "))   
+# b=int(input("Unesite drugi ceo broj: "))
+# a,b = b,a
+# print(a, b)                  #1.6
+
+
+# a = int(input("unesite ceo broj: "))
+# b= float(input("Unesite realan broj: "))
+# c= (input("unesite tekst: "))
+
+# list = [a,b,c]
+# print(list)
+
+# a1=float(a)
+# b1=int(b)
+# c1 = c
+
+# list2=[a1,b1,c1]
+# print(list2)
+
+# a2= str(int(a))
+# b2 = str(int(b))
+# c2 = c
+
+# list3= [a2,b2,c]
+# print(list3)                  # 1.62
+
+# c= int(input("unesite prvi broj: "))
+# d= int(input("unesite drugi broj: "))
+
+# print("Vrednost izraza: a>b: ", c > d)
+# print("Vrednost izraza a<b: ", c < d)
+# print("Vrednost izraza a!=b: ", c != d)
+# print("Vrednost izraza a=b: ", c == d)         #1.7
+
+# a= float(input("unesi broj: "))
+
+# if a > 0:
+#     print("veci je od nule")
+# else:
+#     print("manji je od nule")            #1.8
+
+# a= int(input("unesite broj: "))
+
+# if a >0 :
+#     print("veci je od nule")
+# if a <10:
+#     print("manji od 10")
+# else:
+#     print("broj je veci od 0 i veci od 10")       #1.9
+
+
+# a=int(input("Unesite ceo broj: "))  
+# print(a!=5)                                 #1.10
+
+
+# a=int(input("Unesite ceo broj: "))
+# if a%2==0:
+#     print("da")
+# else:
+#     print("ne")                  #1.11
+
+
+# a= int(input("unesite realan broj: "))
+
+# if (a>0 and a<10):
+#     print("yes")
+# else:
+#     print("no")            #2.3
+
+
+# a= int(input("unesite realan broj: "))
+
+# if a < 0 or a==0:
+#     print("manje od 0")
+# else:
+#     print("veci od 0")
+# if a >=1 and a <10:
+#     print("jednocifren")
+# elif a >=10 and a<100:
+#     print("dvocifren")
+# elif a >=100 and a<1000:
+#     print("trocifren: ")
+# else:
+#     print("veci od 1000")          #2.4
+
+# jedinica = range(1,50)
+# dvojka = range(51,60)
+# trojka = range(61,70)
+# cetvorka = range(71,80)
+# petica = range(81,100)
+
+# a= int(input("unesite broj bodova: "))
+
+# if a == jedinica:
+#     print("kec")
+# elif a == dvojka:
+#     print("dvojka")
+# elif a == trojka:
+#     print("trojka")
+# elif a == cetvorka:
+#     print("cetvorka")
+# elif a == petica:
+#     print("bravo")
+
+
+# a= int(input("unesite broj bodova: "))
+# kec = (a>=1 and a<=10)
+# dva =(a>=11 and a<=20)
+# tri =(a>=21 and a<=30)
+# cetri=(a>=31 and a<=40)
+# pet=(a>=41 and a<=50)
+
+
+# if (a == kec):
+#     print("kec")
+# elif (a == dva):
+#     print("dva")
+# elif (a == tri):
+#     print("tri")
+# elif (a==cetri):
+#     print("cetri")
+# elif (a== pet):
+#     print("pet")
+# else:
+#     print("jedi govna")
+    
+# if ((a>=1 and a<=10)):
+#     print("kec")
+# elif (a>=11 and a<=20):
+#     print("dva")
+# elif (a>=21 and a<=30):
+#     print("tri")
+# elif (a>=31 and a<=40):
+#     print("cetri")
+# elif (a>=41 and a<=50):
+#     print("pet")
+# else:
+#     print("jedi govna")           #2.5
+
+# a = int(input("unesite prvi broj: "))
+# b = int(input("unesite drugi broj: "))
+
+# if a>b:
+#     print("veci je", b)
+# else:
+#     print("manji je",a)
+
+# if a>b:
+#     b==manj_broj
+#     print("veci je", manj_broj)
+# else:
+#     print("manji je",a)                   #2.6
+
+# a = int(input("unesite prvi broj: "))
+# b = int(input("unesite drugi broj: "))
+# c = int(input("unesite treci broj: "))
+
+# if a<b and a<c:
+#     print("najmanji je a: ", a)
+# elif b<c and b<a:
+#     print("najmanji je b: ", b)
+# elif c<a and c<b:
+#     print("najmanji je c: ", c)
+# else:
+#     print("greska")                       #2.7
+
+# for ika in range(10):
+#     print(ika)
+
